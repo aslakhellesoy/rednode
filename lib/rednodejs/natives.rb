ac9dd4f253c4ea444132daf29234e95b93a1d453
aslakhellesoy <aslak.hellesoy@gmail.com>
2010-05-24 16:09:16 +0200

    Cache javascript

--- lib/rednodejs/natives.rb
+++ lib/rednodejs/natives.rb
@@ -1,9 +1,11 @@
 module Rednodejs
   class Natives
+    @@scripts = {}
+    
     Dir["#{ENV['NODE_HOME']}/lib/*.js"].each do |native_js|
       attribute = File.basename(native_js, File.extname(native_js)).to_sym
       define_method(attribute) do || # Empty pipes needed to make ruby realize arity is 0.
-        File.read(native_js)
+        @@scripts[attribute] ||= File.read(native_js)
       end
     end
   end
