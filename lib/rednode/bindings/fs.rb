eac3fda71511eafe96c7206d092cbf51cd56be61
Charles Lowell <cowboyd@thefrontside.net>
2010-08-24 13:31:12 -0500

    remove extra spaces

--- lib/rednode/bindings/fs.rb
+++ lib/rednode/bindings/fs.rb
@@ -11,14 +11,14 @@ module Rednode::Bindings
         @descriptors[fd] = file
       end
     end
-    
+
     def close(fd)
       file(fd) do |f|
         f.close()
         @descriptors.delete(fd)
       end
     end
-    
+
     def read(fd, buffer, offset, length, position)
       file(fd) do |f|
         raise "Second argument needs to be a buffer" unless buffer.kind_of?(Buffer::Buffer)
@@ -39,23 +39,23 @@ module Rednode::Bindings
         file.write(data[offset, length])
       end
     end
-    
+
     def stat(path)
       Stats.new(path)
     end
-    
+
     class Stats
       def initialize(path)
         @stat = File.new(path).stat
       end
-      
+
       def size
         @stat.size
       end
     end
-    
+
     private
-    
+
     def file(fd)
       if file = @descriptors[fd]
         yield file
